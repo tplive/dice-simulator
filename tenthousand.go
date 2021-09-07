a84cfa7ba25506e5065e82e10d2483b34bd65f96
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-09-07 19:12:52 +0200

    Can iterate over x games, and figure out the average. Still waaay too high, and seems output is off as well.

--- tenthousand.go
+++ tenthousand.go
@@ -7,14 +7,14 @@ import (
 )
 
 func main() {
-	//iterate()
-	playOne()
+	iterate()
+	//playOne()
 }
 
 func playOne() {
+	var s simulator.GameData
 	//s.SetAggressive(0.8)
 	//s.IgnoreTripleTwos(0.5)
-	var s simulator.GameData
 
 	s.SetMinToKeep(0)
 	s.SetWhenToQuit(3)
@@ -23,25 +23,26 @@ func playOne() {
 }
 
 func iterate() {
+	var s simulator.GameData
 	//s.SetAggressive(0.8)
 	//s.IgnoreTripleTwos(0.5)
 
 	avgRoundsToWin := 0.0
 	totalRounds := 0
 	counter := 0
-	games := 300
+	games := 100
 
 	for counter < games {
-		var s simulator.GameData
 
 		s.SetMinToKeep(0)
 		s.SetWhenToQuit(3)
 		s.PlayRounds()
 		totalRounds += s.GetRounds()
 		counter++
+		s.ResetSimulator()
 	}
 	avgRoundsToWin = float64(totalRounds) / float64(counter)
 
-	fmt.Printf("Gjennomsnittlig antall runder for å nå 10000, kjører %v spill: %.2f", games, avgRoundsToWin)
+	fmt.Printf("Gjennomsnittlig antall runder for å nå 10000, kjører %v spill: %.2f\n", games, avgRoundsToWin)
 	//fmt.Printf("Antall runder: %v, sum: %v", s.GetRounds(), s.GetTotalPoints())
 }
